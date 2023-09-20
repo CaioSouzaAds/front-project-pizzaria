a937ab6de136ef2348900fcf3963d04bbd6de20e
CaioSouzaAds <caio.cesarads@gmail.com>
2023-09-20 14:36:59 -0300

    creating login screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+import { Input } from "../components/ui/Input";
+
+import logoImg from "../../public/logo.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/contato">Contato</Link>
-    </div>
+    <>
+      <Head>
+        <title>Pizzaria Sabores da Sicília - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo pizzaria" />
+      </div>
+
+      <div className={styles.login}>
+        <form>
+          <Input placeholder="Digite seu email" type="text" />
+
+          <Input placeholder="Sua senha" type="password" />
+        </form>
+      </div>
+    </>
   );
 }

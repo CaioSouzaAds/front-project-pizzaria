ac06bacd09ebc4389c3aca8764839e29b87fa8e0
CaioSouzaAds <caio.cesarads@gmail.com>
2023-09-22 17:10:59 -0300

    registration page created

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,26 +1,30 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
-import styles from "@/styles/home.module.scss";
-import { Input } from "../components/ui/Input";
+import styles from "../../styles/home.module.scss";
+import { Input } from "../../components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import { useState } from "react";
 
-import logoImg from "../../public/newlogo.png";
+import logoImg from "../../../public/newlogo.png";
 
-export default function Home() {
+export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <>
       <Head>
-        <title>Pizzaria Sabores da Sicília - Faça seu login</title>
+        <title>Faça seu cadastro agora</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image className={styles.img} src={logoImg} alt="Logo pizzaria" />
 
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
+
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
+
             <Input placeholder="Digite seu email" type="text" />
 
             <Input
@@ -38,12 +42,12 @@ export default function Home() {
             </label>
 
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link className={styles.text} href={"/signup"}>
-            Não possui uma conta? Cadastra-se
+          <Link className={styles.text} href={"/"}>
+            Já possui uma conta? Faça login!
           </Link>
         </div>
       </div>

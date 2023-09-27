d1b5a4721da29b4a97752335b6edd1b0b6ee3692
CaioSouzaAds <caio.cesarads@gmail.com>
2023-09-27 15:18:39 -0300

    Implement logged-in functionality for guest user routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,8 @@ import { toast } from 'react-toastify';
 
 import logoImg from '../../public/newlogo.png';
 
+import { canSSRGuest } from '../utils/canSSRGuest';
+
 export default function Home() {
   const { signIn } = useContext(AuthContext);
 
@@ -92,3 +94,9 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
---
+++ src/utils/canSSRGuest.ts
@@ -0,0 +1,23 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
+
+import { parseCookies } from 'nookies';
+
+// Function for pages that only visitors have access to.
+
+export function canSSRGuest<P extends { [key: string]: any }>(fn: GetServerSideProps<P>) {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    // Se se o usuário tentar acessar a pagina porem tendo já um login salvo redirecionamos
+
+    if (cookies['@nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/dashboard',
+          permanent: false,
+        },
+      };
+    }
+
+    return await fn(ctx);
+  };
+}
